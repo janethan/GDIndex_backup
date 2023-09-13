3f445f5b8d792ba7c6c82d6792d95d7a79d801cc
janethan <lichuanjie@gmail.com>
2023-09-13 21:08:05 +0800

    Update worker.js

--- build/worker.js
+++ build/worker.js
@@ -6,10 +6,10 @@ self.props = {
   "refresh_token": "",
   service_account: false, 
   service_account_json: {},
-  auth: false, //  默认关闭验证，改成`true`启用验证后用户名和密码才会生效
-  "user": "", // 访问索引验证的用户名
-  "pass": "", // 访问索引验证的密码
-  upload: true, // 默认已开启上传功能，取消上传功能请改成`false`
+  "auth": false, //  默认关闭验证，改成`true`启用验证后用户名和密码才会生效
+  "user": "", // 右侧双引号内是 用户名
+  "pass": "", // 右侧双引号内是 密码
+  "upload": "true", // 默认已开启上传功能，取消上传功能请改成`false`
   lite: false
 };
 (function () {
